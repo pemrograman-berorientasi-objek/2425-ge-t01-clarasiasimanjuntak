c498bab3f5a18291871509ed63641b0a639e7ab2
Clarasia simanjuntak <143679140+clarasiasimanjuntak@users.noreply.github.com>
2025-02-07 18:05:14 +0700

    task 2

--- src/fintech/model/Transaction.java
+++ src/fintech/model/Transaction.java
@@ -1,7 +1,7 @@
 package fintech.model;
 
 public class Transaction {
-    private static int counter = 1;  // ID transaksi otomatis
+    private static int counter = 1; 
     
     private int id;
     private String accountName;
@@ -9,36 +9,26 @@ public class Transaction {
     private String postedAt;
     private String note;
 
-    // Konstruktor
     public Transaction(String accountName, double amount, String postedAt, String note) {
-        this.id = counter++;  // Set ID transaksi dan naikkan counter
+        this.id = counter++; 
         this.accountName = accountName;
         this.amount = amount;
         this.postedAt = postedAt;
         this.note = (note.length() > 40) ? note.substring(0, 40) : note; // Batasi note max 40 karakter
     }
 
-    // Getter untuk Account Name
     public String getAccountName() {
         return accountName;
     }
-
-    // Getter untuk saldo transaksi
     public double getAmount() {
         return amount;
     }
-
-    // Getter untuk waktu transaksi
     public String getPostedAt() {
         return postedAt;
     }
-
-    // Getter untuk catatan transaksi
     public String getNote() {
         return note;
     }
-
-    // Format output sesuai spesifikasi
     @Override
     public String toString() {
         return id + "|" + accountName + "|" + String.format("%.1f", amount) + "|" + postedAt + "|" + note;
